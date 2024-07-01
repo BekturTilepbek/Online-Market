62439bc79c8ddbd4ca80632a379cfbb87bf80542
Bektur <project.bekturtilepbek@gmail.com>
2024-07-01 15:16:45 +0600

    создал модели категорий и продуктов, зарегистрировал их, провел миграции

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Category, Product
+
+admin.site.register(Category)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'category']
+    list_display_links = ['id', 'name']
+    list_filter = ['category']
+    search_fields = ['name']
+    fields = ['name', 'description', 'category', 'created_at', 'price']
+
+
+admin.site.register(Product, ProductAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True, verbose_name="Наименование")
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")
+
+    def __str__(self):
+        return f"{self.pk}. {self.name}"
+
+    class Meta:
+        db_table = "categories"
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Наименование")
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")
+    category = models.ForeignKey(
+        "webapp.Category",
+        on_delete=models.RESTRICT,
+        verbose_name="Категория",
+        related_name="products",
+        null=False,
+        blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время добавления")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Стоимость")
+    image = models.URLField(max_length=300)
+
+    def __str__(self):
+        return f"{self.pk}. {self.name} ({self.category}): {self.price}"
+
+    class Meta:
+        db_table = "products"
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
