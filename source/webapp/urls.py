8d66aa3e51388a131048713e5fdaebef533812b6
Bektur <project.bekturtilepbek@gmail.com>
2024-07-01 17:41:38 +0600

    добавил возможность добавления новых продуктов и категорий
    подправил верстку

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from webapp.views import index, detail_product
+from webapp.views import index, detail_product, new_product, new_category
 
 urlpatterns = [
     path('', index, name='products'),
     path('products/', index, name='products'),
-    path('products/<int:product_id>/', detail_product, name='detail_product'),
+    path('products/<int:pk>/', detail_product, name='detail_product'),
+    path('products/add/', new_product, name='new_product'),
+    path('categories/add/', new_category, name='new_category')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
-from webapp.models import Product
+from webapp.models import Product, Category
 
 
 def index(request):
@@ -8,6 +8,33 @@ def index(request):
     return render(request, 'index.html', context={'products': products})
 
 
-def detail_product(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
+def detail_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
     return render(request, 'detail_product.html', context={'product': product})
+
+
+def new_product(request):
+    if request.method == "GET":
+        return render(request, 'new_product.html', context={'categories': Category.objects.all()})
+    else:
+        product = Product.objects.create(
+            name=request.POST.get('name'),
+            description=request.POST.get('description') if request.POST.get('description') else None,
+            category=get_object_or_404(Category, pk=request.POST.get('category')),
+            price=request.POST.get('price'),
+            image=request.POST.get('image')
+        )
+
+        return redirect('detail_product', pk=product.pk)
+
+
+def new_category(request):
+    if request.method == "GET":
+        return render(request, 'new_category.html')
+    else:
+        Category.objects.create(
+            name=request.POST.get('name'),
+            description=request.POST.get('description') if request.POST.get('description') else None
+        )
+
+        return redirect('products')
