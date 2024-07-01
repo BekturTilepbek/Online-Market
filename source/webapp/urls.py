3f876e516d67bdc63e00e5454662f18dcce3cb2a
Bektur <project.bekturtilepbek@gmail.com>
2024-07-01 16:20:59 +0600

    добавил детальный просмотр товара

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from webapp.views import index
+from webapp.views import index, detail_product
 
 urlpatterns = [
     path('', index, name='products'),
+    path('products/', index, name='products'),
+    path('products/<int:product_id>/', detail_product, name='detail_product'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from webapp.models import Product
 
@@ -6,3 +6,8 @@ from webapp.models import Product
 def index(request):
     products = Product.objects.all()
     return render(request, 'index.html', context={'products': products})
+
+
+def detail_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'detail_product.html', context={'product': product})
